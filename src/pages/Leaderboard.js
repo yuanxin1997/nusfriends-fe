5c9c1d24a657a4ce55138054298fcc81fa65c878
yuanxin <yuanxintaxon@gmail.com>
2021-10-23 10:31:57 +0800

    leader board list

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-import { Col, Row } from "antd";
+import { List, Avatar, Col, Row, Card, Skeleton, Divider, message } from "antd";
 import CircleCard from "../components/CircleCard";
 
 import styled from "styled-components";
@@ -10,6 +11,30 @@ import ContainerHeader from "../components/ContainerHeader";
 const { Header, Footer, Sider, Content } = Layout;
 
 const Leaderboard = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const loadMoreData = () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    fetch(
+      "https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo"
+    )
+      .then((res) => res.json())
+      .then((body) => {
+        setData([...data, ...body.results]);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    loadMoreData();
+  }, []);
   /* START -- SETUP FOR COMPONENT */
   const tabData = [
     {
@@ -30,32 +55,122 @@ const Leaderboard = () => {
       {
         name: "My Circles",
         path: "/my-circles",
-      }, 
+      },
       {
         name: "to be removed, fetch data and push here",
         path: "this can be empty",
-      }
+      },
     ],
   };
   /* END -- SETUP FOR COMPONENT */
 
+  const leaderBoardData = [
+    {
+      photoUrl: "url",
+      name: "CommentOutlined",
+      likes: 5,
+    },
+    {
+      photoUrl: "url",
+      name: "CommentOutlined",
+      likes: 5,
+    },
+    {
+      photoUrl: "url",
+      name: "CommentOutlined",
+      likes: 5,
+    },
+  ];
+
+  const tdata = [
+    {
+      title: "Ant Design Title 1",
+    },
+    {
+      title: "Ant Design Title 2",
+    },
+    {
+      title: "Ant Design Title 3",
+    },
+    {
+      title: "Ant Design Title 4",
+    },
+  ];
   return (
     <Layout style={{ height: "100vh", backgroundColor: "var(--accent-bg)" }}>
       <Sider style={{ backgroundColor: "var(--accent-bg)" }}>
         <SideBar tabData={tabData} />
       </Sider>
-      <Content style={{ backgroundColor: "var(--accent-bg)" }}>
+      <Content style={{ backgroundColor: "var(--accent-bg)", marginRight: "10%" }}>
         <Row justify="start">
           <Col>
             <ContainerHeader headData={headData} />
           </Col>
         </Row>
         <Row>
- 
+          <Col span={24}>
+            <Wrapper>
+              <ListWrapper>
+                <div
+                  id="scrollableDiv"
+                  style={{
+                    height: 400,
+                    overflow: "auto",
+                    padding: "0 16px"
+                  }}
+                >
+                  <InfiniteScroll
+                    dataLength={data.length}
+                    next={loadMoreData}
+                    hasMore={data.length < 50}
+                    loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+                    endMessage={
+                      <Divider plain>It is all, nothing more 🤐</Divider>
+                    }
+                    scrollableTarget="scrollableDiv"
+                  >
+                    <List
+                      dataSource={data}
+                      renderItem={(item) => (
+                        <List.Item key={item.id}>
+                          <List.Item.Meta
+                            avatar={<Avatar src={item.picture.large} />}
+                            title={
+                              <a href="https://ant.design">{item.name.last}</a>
+                            }
+                          />
+                          <div>Content</div>
+                        </List.Item>
+                      )}
+                    />
+                  </InfiniteScroll>
+                </div>
+              </ListWrapper>
+            </Wrapper>
+          </Col>
         </Row>
       </Content>
     </Layout>
   );
-}
+};
 
 export default Leaderboard;
+
+const Wrapper = styled.div`
+  height: 600px;
+  background-color: white;
+  /* transition: var(--transition); */
+  font-weight: 400;
+`;
+const ListWrapper = styled.div`
+
+`;
+
+// const ItemWrapper = styled.div`
+//   border: 1px red solid;
+//   display: flex;
+//   height: 50px;
+//   justify-content: flex-start;
+//   padding: 1em 2em 1em;
+//   align-items: center;
+// `;
