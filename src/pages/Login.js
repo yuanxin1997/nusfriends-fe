1492af0599ccb1fa28ef25bf43d5f5aac48068da
lalalawson <lawson.tkw@gmail.com>
2021-10-24 18:51:36 +0800

    updated login page to store jwt and userid in localstorage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,18 @@
-import { Button, Col, Input, Row, Form } from "antd";
+import { Button, Col, Input, Row, Form, message } from "antd";
 import Password from "antd/lib/input/Password";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { Url } from "../constants/global";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 function Login() {
     const history = useHistory();
     const [pending, setPending] = useState(false);
     const [error, setError] = useState(null);
 
+    // login function
     const onFinish = async (values) => {
         const user = {
             email: values.email,
@@ -22,9 +24,12 @@ function Login() {
         await axios
             .post(`${Url}/users/login`, { user })
             .then((res) => {
-                // TODO: save the returned jwt
                 console.log(res);
                 setPending(false);
+                var jwt = res.data.jwtToken;
+                var userId = jwt_decode(jwt).user.userId;
+                localStorage.setItem("jwt", jwt);
+                localStorage.setItem("userId", userId);
                 history.push("/");
             })
             .catch(function (error) {
@@ -43,6 +48,16 @@ function Login() {
                 console.log(error.config);
             });
     };
+
+    // check if user is already logged in
+    useEffect(() => {
+        if (localStorage.jwt === undefined) {
+            return "";
+        } else {
+            history.push("/");
+            message.success("Logged in!");
+        }
+    }, []);
 
     return (
         <div
