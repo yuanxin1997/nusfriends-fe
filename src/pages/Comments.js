fcdbb6a354a292e1b6cf0a4c56a3661d977ac89c
Marcus <marcusmljw98@gmail.com>
2021-11-12 10:36:12 +0800

    fix post in comments timestamp bug

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -172,6 +172,7 @@ function Comments(props) {
                   posterId={post.userid}
                   tags={post.tags}
                   postType={post.posttype}
+                  postedDate={post.createdat}
                   comments={comments}
                 />
               ) : (
