e692fff70147f72ae370fcbdee70a7b54888d72e
lalalawson <lawson.tkw@gmail.com>
2021-11-12 12:29:34 +0800

    deploy ws round4)

--- src/helpers/websoc.js
+++ src/helpers/websoc.js
@@ -2,7 +2,7 @@ import WebSocket from "isomorphic-ws";
 
 export const wsTrigger = (id) => {
     const websocket = new WebSocket(
-        `ws://nusfriends-be.herokuapp.com/nus-friends?userId=${id}`
+        `wss://nusfriends-be.herokuapp.com/nus-friends?userId=${id}`
     );
     websocket.onopen = () => {
         websocket.send(JSON.stringify(id));
